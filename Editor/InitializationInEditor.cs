05234eba2fa19d9cbfabff44b7da9c78496912b3
dgw0103 <dgw0103@naver.com>
2024-11-09 22:46:07 +0900

    Update InitializationInEditor.cs

--- Editor/InitializationInEditor.cs
+++ Editor/InitializationInEditor.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -8,15 +7,39 @@ using System.IO;
 using System.Threading;
 using System;
 using System.Threading.Tasks;
+using System.Linq;
+using UnityEditor.PackageManager.UI;
+using UnityEditor.TextCore.Text;
+using UnityEditor.TextCore;
 
 [InitializeOnLoad]
 public class InitializationInEditor
 {
     static InitializationInEditor()
     {
-        RunPackageListTask();
+        if (SessionState.GetBool(SessionKey, default))
+        {
+            return;
+        }
+
+        SessionState.SetBool(SessionKey, true);
+        Initialize();
     }
-    private static async void RunPackageListTask()
+
+
+
+    private static string SessionKey { get => typeof(InitializationInEditor).Assembly + typeof(InitializationInEditor).FullName; }
+    private static async void Initialize()
+    {
+        bool isPackageInstalled = await IsPackageInstalled();
+
+        if (isPackageInstalled == false)
+        {
+            Debug.Log("add unity utility");
+            Client.Add("https://github.com/dgw0103/UnityUtility.git");
+        }
+    }
+    private static async Task<bool> IsPackageInstalled()
     {
         try
         {
@@ -26,17 +49,15 @@ public class InitializationInEditor
             if (resultStatus != StatusCode.Success)
             {
                 Debug.LogError("Failed to retrieve package list: " + listRequest.Error.message);
-                return;
+                return false;
             }
 
-            foreach (var package in listRequest.Result)
-            {
-                Debug.Log(package.name);
-            }
+            return listRequest.Result.Any((x) => x.name == "com.dgw0103.addressablesutility");
         }
         catch (Exception ex)
         {
             Debug.LogError("An error occurred: " + ex.Message);
+            return false;
         }
     }
     private static async Task<StatusCode> WaitForRequestCompletion(ListRequest request)
